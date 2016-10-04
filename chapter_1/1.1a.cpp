346f6caaf8d811fb2de8cccb7613053b53b1f4b6
Gavin Thomas Claugus <gclaugus@gmail.com>
2016-10-04 10:31:10 -0400

    Improve documentation for 1.1a solution
    Signed-off-by: Gavin Thomas Claugus <gclaugus@gmail.com>

--- chapter_1/1.1a.cpp
+++ chapter_1/1.1a.cpp
@@ -1,4 +1,9 @@
 /*
+ * PROBLEM:
+ * Implement an algorithm to determine if a string has all unique characters.
+ * You may use additional data structures.
+ *
+ * IMPLEMENTATION:
  * Iterate through the string, placing every character in a set. Then check to
  * see if the size of the set is the same as the size of the string.
  *
@@ -11,6 +16,14 @@
  * SPACE COMPLEXITY: O(1)
  * We take up O(1) space since we use chars. A char can have 256
  * different values, so our set has no more that 256 elements.
+ *
+ * HOW COULD YOU IMPROVE THIS?
+ * Using a hash table to store character counts would work, though it would
+ * still have worst-case O(n^2) complexity.
+ *
+ * Storing character counts in a 128-element array indexed by character seems
+ * to be the best solution. It would bring our time complexity down to O(n).
+ * See 1.1b.cpp for that implementation.
  */
 
 #include <set>
