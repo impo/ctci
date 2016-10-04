2302b4081d3772f100127b728044f4db47379fa7
Gavin Thomas Claugus <gclaugus@gmail.com>
2016-10-04 10:08:27 -0400

    Add complexity documentation for 1.1a
    Signed-off-by: Gavin Thomas Claugus <gclaugus@gmail.com>

--- chapter_1/1.1a.cpp
+++ chapter_1/1.1a.cpp
@@ -1,3 +1,18 @@
+/*
+ * Iterate through the string, placing every character in a set. Then check to
+ * see if the size of the set is the same as the size of the string.
+ *
+ * TIME COMPLEXITY: O(n * log(n))
+ * std::set is usually implemented as an RB-tree, so insertion should take
+ * O(log(n)) time on a typical implementation. Since we iterate over every
+ * element of the string, we get O(n * log(n)) time complexity for this
+ * solution.
+ *
+ * SPACE COMPLEXITY: O(1)
+ * We take up O(1) space since we use chars. A char can have 256
+ * different values, so our set has no more that 256 elements.
+ */
+
 #include <set>
 #include <string>
 #include <iostream>
