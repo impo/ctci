d3dd962447325a2dd0b96c1aa94667f4c3cef874
Gavin Thomas Claugus <gclaugus@gmail.com>
2016-10-08 13:41:12 -0400

    Add writeup for 1.6
    Signed-off-by: Gavin Thomas Claugus <gclaugus@gmail.com>

--- chapter_1/1.6.cpp
+++ chapter_1/1.6.cpp
@@ -1,3 +1,41 @@
+/*
+ * PROBLEM: Implement a method to perform basic string compression using the counts of
+ * repeated characters. For example, the string aabcccccaaa would because a2b1c5ae. If
+ * the "compressed" string would not become smaller than the original string, your method
+ * should return the original string. You can assume the string has only uppercase and
+ * lowercase letters.
+ *
+ * IMPLEMENTATION:
+ * If the string is empty, immediately return the string. Next, iterate through the
+ * string, keeping count of how many instances of a character arise in a row as you
+ * iterate.
+ *
+ * Create a new string to hold the compressed version of the original string.
+ * Every time you encounter a new character, append the old character, along with its
+ * count, to the new string.
+ *
+ * After iteration, do not forget to append the last character
+ * along with its count.
+ *
+ * After appending the last character and its count, compare the size of the compressed
+ * string to that of the original string. If the new string is longer than or as long as
+ * the original string, return the original string. Otherwise, return the new string.
+ *
+ * TIME COMPLEXITY:
+ * O(n), as we iterate through the entire old string, performing constant work with each
+ * iteration.
+ *
+ * SPACE COMPLEXITY:
+ * O(n). In the worst case, every character in the old string will be different from its
+ * predecessor. Then our compressed string will append a count of 1 after each character,
+ * storing 2n characters in the new string and n characters in the old string for roughly
+ * 3n storage. We drop the constant to get O(n).
+ *
+ * WHAT COULD YOU IMPROVE?
+ * I might be able to take advantage of the fact that the string can only contain
+ * alphabetical characters. There may be some way to drop the space complexity of my
+ * answer, though I'm not sure what that might be.
+ */
 #include <string>
 #include <iostream>
 
